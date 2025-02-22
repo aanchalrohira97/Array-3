521b5e24f602c54b66d6e8d1e311a9223501ce39
Aanchal Rohira <aanchalrohira@Aanchals-MacBook-Pro.local>
2025-02-22 15:30:17 -0500

    Array-3 completed, please review

---
+++ hIndex.java
@@ -0,0 +1,16 @@
+// tc: O(n)
+// sc: O(n)
+
+public class Solution {
+
+  public int hIndex(int[] citations) {
+    int n = citations.length;
+    int[] papers = new int[n + 1];
+    // counting papers for each citation number
+    for (int c : citations) papers[Math.min(n, c)]++;
+    // finding the h-index
+    int k = n;
+    for (int s = papers[n]; k > s; s += papers[k]) k--;
+    return k;
+  }
+}
